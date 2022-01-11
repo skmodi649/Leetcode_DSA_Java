4a0661a7fd694f9784fd150858ee0cf0bf3e944b
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-01-12 01:17:03 +0530

    Package Array : Maximum value of equation program commit

---
+++ src/Array/Max_Value_Equation.java
@@ -0,0 +1,11 @@
+// Difficulty : Hard
+package Array;
+
+public class Max_Value_Equation {
+    public int findMaxValueOfEquation(int[][] points, int k) {
+
+    }
+    public static void main(String[] args){
+
+    }
+}
